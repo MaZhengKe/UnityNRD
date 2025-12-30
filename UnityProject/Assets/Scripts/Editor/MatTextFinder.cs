b3abed4096ca3954d8af9c22c44233fda91863fa
mk <ma_zhengke@outlook.com>
2025-12-30 15:41:28 +0800

    材质补全

--- UnityProject/Assets/Scripts/Editor/MatTextFinder.cs
+++ UnityProject/Assets/Scripts/Editor/MatTextFinder.cs
@@ -23,11 +23,21 @@ namespace PathTracing
         private void FillMaterials()
         {
             var allMaterials = Selection.GetFiltered<Material>(SelectionMode.Editable | SelectionMode.Assets);
+            
+            Debug.Log($"选中材质数量: {allMaterials.Length}");
             int count = 0;
             foreach (var mat in allMaterials)
             {
                 var baseTex = mat.GetTexture("_BaseMap");
+                
+                if(baseTex == null)
+                {
+                    Debug.LogWarning($"材质 {mat.name} 没有_BaseMap贴图，跳过");
+                    continue;
+                }
                 var texName = baseTex.name;
+                 
+                Debug.Log($"检查材质 {mat.name} 的贴图 {texName}");
                 if (texName.Contains("_BaseColor"))
                 {
                     texName = texName.Replace("_BaseColor", "");
@@ -35,13 +45,19 @@ namespace PathTracing
                     
                     var path = AssetDatabase.GetAssetPath(baseTex);
                     var dir = System.IO.Path.GetDirectoryName(path);
-                    var maskTexPath = System.IO.Path.Combine(dir, maskTexName + ".png");
+                    var maskTexPath = System.IO.Path.Combine(dir, maskTexName + ".dds");
+                    Debug.Log($"尝试加载贴图 {maskTexPath}");
+                    
                     var maskTex = AssetDatabase.LoadAssetAtPath<Texture2D>(maskTexPath);
                     if (maskTex != null)
                     {
-                        mat.SetTexture("_SpecGlossMap", maskTex);
+                        mat.SetTexture("_MetallicGlossMap", maskTex);
                         Debug.Log($"材质 {mat.name} 补全贴图 {maskTexName}");
                         count++;
+                    }
+                    else
+                    {
+                        Debug.LogWarning($"未找到贴图 {maskTexPath}，无法补全材质 {mat.name}");
                     }
                     
                 }
