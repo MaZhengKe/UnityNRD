48069801bc1f76e5c8f2b3dd8dc6b6d8e57b8c5f
mk <ma_zhengke@outlook.com>
2026-02-12 13:40:37 +0800

    ForceUpdateTransform

---
+++ UnityProject/Assets/Scripts/ForceUpdateTransform.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class ForceUpdateTransform : MonoBehaviour
+    {
+        private void LateUpdate()
+        {
+            var s = 1 + 0.01f * Mathf.Sin(Time.time * 10);
+            transform.localScale = new Vector3(s, s, s);
+        }
+    }
+}
